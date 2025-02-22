8f6aa3c57c76a387a18a6b152af76831bba514d2
Angelos Nikolopoulos <anikolop@easydev.eu>
2025-02-22 16:04:22 +0200

    Revert VCM back

--- src/settings-ui/Settings.UI/SettingsXAML/OOBE/Views/OobeShellPage.xaml.cs
+++ src/settings-ui/Settings.UI/SettingsXAML/OOBE/Views/OobeShellPage.xaml.cs
@@ -173,7 +173,11 @@ namespace Microsoft.PowerToys.Settings.UI.OOBE.Views
                 ModuleName = "TextExtractor",
                 IsNew = false,
             });
-
+            Modules.Insert((int)PowerToysModules.VideoConference, new OobePowerToysModule()
+            {
+                ModuleName = "VideoConference",
+                IsNew = false,
+            });
             Modules.Insert((int)PowerToysModules.MeasureTool, new OobePowerToysModule()
             {
                 ModuleName = "MeasureTool",
@@ -286,6 +290,7 @@ namespace Microsoft.PowerToys.Settings.UI.OOBE.Views
                     case "FileExplorer": NavigationFrame.Navigate(typeof(OobeFileExplorer)); break;
                     case "ShortcutGuide": NavigationFrame.Navigate(typeof(OobeShortcutGuide)); break;
                     case "TextExtractor": NavigationFrame.Navigate(typeof(OobePowerOCR)); break;
+                    case "VideoConference": NavigationFrame.Navigate(typeof(OobeVideoConference)); break;
                     case "MouseUtils": NavigationFrame.Navigate(typeof(OobeMouseUtils)); break;
                     case "MouseWithoutBorders": NavigationFrame.Navigate(typeof(OobeMouseWithoutBorders)); break;
                     case "MeasureTool": NavigationFrame.Navigate(typeof(OobeMeasureTool)); break;
